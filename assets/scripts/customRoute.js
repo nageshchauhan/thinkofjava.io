ed20321903de0e834d07cf5a183a618f76aec460
Nagesh Chauhan <nageshchauhan1993@gmail.com>
2024-12-21 20:36:08 +0100

    removed html code

--- assets/scripts/customRoute.js
+++
@@ -1,8 +0,0 @@
-myApp.config(function($routeProvider){
-  
-  
-  $routeProvider
-    .when('/menu/:menuName/:fileName',{templateUrl: function(urlAttr){return '../../views/'+urlAttr.menuName+'/'+urlAttr.fileName+'.html'}, controller:'ContentController'})
-  .when('/intro',{templateUrl:'../../views/Introduction.html', controller:'ContentController'})
-    .otherwise({redirectTo:'/intro'});
-});
